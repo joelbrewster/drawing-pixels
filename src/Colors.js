02c8fb0290aa07907c89fe5e6e661d37773e4557
Joel Brewster <hi@joelbrewster.com>
2021-06-10 08:46:44 +1000

    init

---
+++ src/Colors.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default ['white', 'black', 'grey', 'brown', 'goldenrod', 'orange', 'yellow', 'pink', 'red', 'darkblue', 'blue', 'darkgreen', 'green', 'lime', 'fuchsia']
